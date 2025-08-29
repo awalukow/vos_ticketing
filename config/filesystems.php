e7fcb031af2719e65cb6281a5b797fc6cf00b1c7
Axcell <axcellentwalukow@gmail.com>
2025-08-29 16:48:27 +0700

    modified:   config/filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,8 @@ return [
         'public' => [
             'driver' => 'local',
             //'root' => storage_path('app/public'),
-            'root' => storage_path('../public/storage'),
+            //--Local    'root' => storage_path('../public/storage'),
+            'root' => storage_path('../../public_html/ticket.voiceofsoulchoir.id/storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
