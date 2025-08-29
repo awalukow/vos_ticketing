0469124a78e430ac35de60aad3f95c2363f0f2b1
Axcell <axcellentwalukow@gmail.com>
2025-08-29 21:57:41 +0700

    modified:   resources/views/server/laporan/show.blade.php

--- resources/views/server/laporan/show.blade.php
+++ resources/views/server/laporan/show.blade.php
@@ -630,7 +630,7 @@
             <div class="alert alert-info mb-3 p-3">
                 <strong>Instruksi Pembayaran:</strong><br>
                 Bank Jago XXX a.n Ratno Juniarto MS<br>
-                Atau <strong>scan QR dibawah untuk pembayaran via Gopay/OVO/Dana/LinkAja</strong><br>
+                Atau <strong>scan QR dibawah untuk pembayaran via Gopay/OVO/Dana/LinkAja/m-Banking</strong><br>
                 Nominal: <strong>Rp {{ number_format($data->total, 0, ',', '.') }}</strong><br>
                 Batas Waktu Pembayaran: <strong>{{ $paymentExpiry->locale('id')->isoFormat('LLLL') }}</strong>
             </div>
