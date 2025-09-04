13cdd5cf49302c3ccadbc19c4ea967c898dae8b8
Axcell <axcellentwalukow@gmail.com>
2025-09-05 00:29:15 +0700

    modified:   app/Http/Controllers/Auth/RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -148,7 +148,7 @@ untuk informasi lebih lanjut hubungi: https://wa.me/6285823536364 (Jean) atau ht
 
     public function showMaintenance()
     {
-        $customerService = AppSetting::getCustomerService();
+        $maintenanceMode = AppSetting::getCustomerService();
         return view('auth.maintenance', compact('customerService'));
     }
     //public function view_pdf (){
