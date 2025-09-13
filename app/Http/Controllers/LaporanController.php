6411ec45ee16c46e3d026ed11b0ff856d83ecd0a
Axcell <axcellentwalukow@gmail.com>
2025-09-14 01:56:47 +0700

    modified:   app/Http/Controllers/LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Pemesanan;
 use App\Models\Checkin_log;
-use App\Http\Controllers\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
